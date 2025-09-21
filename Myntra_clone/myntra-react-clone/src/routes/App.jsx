7035fc082c1d9aa7f0a945c1f43b070792fe4cb8
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2025-09-21 13:22:27 +0530

    Update App.jsx

--- Myntra_clone/myntra-react-clone/src/routes/App.jsx
+++ Myntra_clone/myntra-react-clone/src/routes/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import FetchItems from "../components/Fetchitems";
+import FetchItems from "../components/FetchItems";
 import { useSelector } from "react-redux";
 import LoadingSpinner from "../components/LoadingSpinner";
 import BagHydrator from "../components/BagHydrator";
