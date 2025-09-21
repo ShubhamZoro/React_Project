a5488b0d3fe95abe1eae1c7f12d4ee7c865db4c7
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2025-09-21 13:07:24 +0530

    Update index.js

--- Myntra_clone/myntra-react-clone/src/store/index.js
+++ Myntra_clone/myntra-react-clone/src/store/index.js
@@ -1,8 +1,8 @@
 import {configureStore} from "@reduxjs/toolkit";
-import itemsSlice from "./itemsSlice";
-import fetchStatusSlice from "./fetchStatusSlice";
-import bagSlice from "./bagSlice";
-import authSlice from "./authSlice";
+import itemsSlice from "./itemsSlice.js";
+import fetchStatusSlice from "./fetchStatusSlice.js";
+import bagSlice from "./bagSlice.js";
+import authSlice from "./authSlice.js";
 const myntraStore = configureStore({
   reducer: {
     items: itemsSlice.reducer,
