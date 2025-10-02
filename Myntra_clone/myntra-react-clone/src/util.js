89bc7053a9f8ea8a03d5cb084a4f2f6edc1cbffb
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2025-10-02 18:18:19 +0530

    Update util.js

--- Myntra_clone/myntra-react-clone/src/util.js
+++ Myntra_clone/myntra-react-clone/src/util.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "https://myntrabackend-blu2r8iq4-shubham-shekhars-projects-b5d38cbe.vercel.app"
+export const API_BASE_URL = "https://myntrabackend-shubhamzoro-shubham-shekhars-projects-b5d38cbe.vercel.app"
